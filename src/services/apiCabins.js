bc9b240bb52f9a5fde70c1ee73fd15b2ad079fd8
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-06-03 00:00:06 +0100

    Create cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,17 @@ export const getCabins = async () => {
   return data;
 };
 
+export const createCabin = async (newCabin) => {
+  const { data, error } = await supabase.from('cabins').insert([newCabin]);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be created');
+  }
+
+  return data;
+};
+
 export const deleteCabin = async (id) => {
   const { data, error } = await supabase.from('cabins').delete().eq('id', id);
 
