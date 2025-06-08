75f86ea7019ff2a5b9746933cba1980b777e3662
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-06-08 22:19:30 +0100

    Table Menu

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -94,8 +94,9 @@ const Row = ({ children }) => {
   );
 };
 
-const Body = ({ children }) => {
-  return children;
+const Body = ({ data, render }) => {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
 };
 
 Table.Header = Header;
