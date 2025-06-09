09f638e6b6cafb505793e8e433f7a5886fb14b38
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-06-09 20:33:07 +0100

    Bookings filtering and sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,29 @@
 import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
 
 export const useBookings = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue, method: 'gte' };
+
+  // Sort
+  const sortByRaw = searchParams.get('sortBy') || 'sortDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isPending, bookings, error };
