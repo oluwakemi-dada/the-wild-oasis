22f94e968f9fc6b413f3e65c6fa4d439d30bed2f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-06-15 19:25:59 +0100

    Login and authorization

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,12 +3,27 @@ import Button from '../../ui/Button';
 import Form from '../../ui/Form';
 import Input from '../../ui/Input';
 import FormRowVertical from '../../ui/FormRowVertical';
+import { useLogin } from './useLogin';
+import SpinnerMini from '../../ui/SpinnerMini';
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('kemi@example.com');
+  const [password, setPassword] = useState('123456');
+  const { login, isPending } = useLogin();
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail('');
+          setPassword('');
+        },
+      }
+    );
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -16,10 +31,10 @@ const LoginForm = () => {
         <Input
           type='email'
           id='email'
-          // This makes this form better for password managers
           autoComplete='username'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isPending}
         />
       </FormRowVertical>
       <FormRowVertical label='Password'>
@@ -29,10 +44,13 @@ const LoginForm = () => {
           autoComplete='current-password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isPending}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size='large'>Login</Button>
+        <Button size='large' disabled={isPending}>
+          {!isPending ? 'Login' : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { useUser } from '../features/authentication/useUser';
+import Spinner from './Spinner';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+
+  const { isPending, user, isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isPending) navigate('/login');
+  }, [isAuthenticated, isPending, navigate]);
+
+  if (isPending)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+};
+
+export default ProtectedRoute;
